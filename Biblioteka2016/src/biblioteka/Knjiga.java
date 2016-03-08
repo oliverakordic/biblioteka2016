52dac59097bbc141a1d8628f6eac5ca9e5288e00
oliverakordic <oljaitacka@gmail.com>
2016-03-08 13:50:43 +0100

    Dodati toString(), equals() i hashCode() u Knjiga i Autor.

--- Biblioteka2016/src/biblioteka/Knjiga.java
+++ Biblioteka2016/src/biblioteka/Knjiga.java
@@ -20,6 +20,11 @@ public class Knjiga {
 	public void setIsbn(long isbn) {
 		this.isbn = isbn;
 	}
+	@Override
+	public String toString() {
+		return "Knjiga [naslov=" + naslov + ", isbn=" + isbn + ", autori=" + autori + ", izdavac=" + izdavac
+				+ ", izdanje=" + izdanje + "]";
+	}
 	public LinkedList<Autor> getAutori() {
 		return autori;
 	}
@@ -37,6 +42,47 @@ public class Knjiga {
 	}
 	public void setIzdanje(int izdanje) {
 		this.izdanje = izdanje;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((autori == null) ? 0 : autori.hashCode());
+		result = prime * result + (int) (isbn ^ (isbn >>> 32));
+		result = prime * result + izdanje;
+		result = prime * result + ((izdavac == null) ? 0 : izdavac.hashCode());
+		result = prime * result + ((naslov == null) ? 0 : naslov.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Knjiga other = (Knjiga) obj;
+		if (autori == null) {
+			if (other.autori != null)
+				return false;
+		} else if (!autori.equals(other.autori))
+			return false;
+		if (isbn != other.isbn)
+			return false;
+		if (izdanje != other.izdanje)
+			return false;
+		if (izdavac == null) {
+			if (other.izdavac != null)
+				return false;
+		} else if (!izdavac.equals(other.izdavac))
+			return false;
+		if (naslov == null) {
+			if (other.naslov != null)
+				return false;
+		} else if (!naslov.equals(other.naslov))
+			return false;
+		return true;
 	}
 
 }
