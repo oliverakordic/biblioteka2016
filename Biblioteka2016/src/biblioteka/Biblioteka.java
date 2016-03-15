70706234a4a2f0da3273adee60e4bf1d19a9bcd5
student1 <student1@RCB001-15.rc.fon.bg.ac.yu>
2016-03-15 13:01:57 +0100

    Uvedena logicka kontrola u metodu pronadjiKnjigu.

--- Biblioteka2016/src/biblioteka/Biblioteka.java
+++ Biblioteka2016/src/biblioteka/Biblioteka.java
@@ -17,6 +17,9 @@ public class Biblioteka implements bibliotekainterfejs{
 	}
 	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, long ISBN,String naslov, String izdavac){
 		LinkedList<Knjiga> rezultat = new LinkedList<Knjiga>();
+		
+		if(naslov==null)
+			return knjige;
 		for(int i=0; i<knjige.size(); i++){
 			if(knjige.get(i).getNaslov().contains(naslov))
 				rezultat.add(knjige.get(i));
